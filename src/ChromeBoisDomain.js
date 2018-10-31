438189fd0beaee60d16147897bc198222cf98fd1
Nate Miller <natekmiller96@gmail.com>
2018-10-31 01:06:37 +0000

    automatically backed up by learn

--- src/ChromeBoisDomain.js
+++ src/ChromeBoisDomain.js
@@ -18,7 +18,7 @@ export default class ChromeBoisDomain extends Component {
 
   render() {
     return (
-      <canvas>
+      <canvas
         onClick = {() => {toggleCycling()}}
         onMouseMove={this.handleMouseMove}
         onKeyPress={this.handleKeyDown}
